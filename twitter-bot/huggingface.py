c82d5e824ca24ce55bca3c3124cf6c025e1c3612
Zayatsoff <rozin.lior@gmail.com>
2022-04-11 23:30:20 -0400

    Attempt to put transformer onto GPU

--- twitter-bot/huggingface.py
+++ twitter-bot/huggingface.py
@@ -1,19 +1,23 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
 import torch
-from torch.functional
 
-model_name = "hakurei/c1-6B-8bit"
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# from torch.functional
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_name = "hivemind/gpt-j-6B-8bit"
+model = AutoModelForQuestionAnswering.from_pretrained(model_name).to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_name).to(device)
 
-generator = pipeline("question-answering", model=model, tokenizer=tokenizer)
+# generator = pipeline("question-answering", model=model, tokenizer=tokenizer).to(device)
 
-response = generator("What is your name?")
+# response = generator("What is your name?").to(device)
 
 # Tokenize the response
-input ="What is your name?"
-batch = tokenizer(input,padding=True,truncation=True,max_length=512,return_tensors="pt")
+input = "What is your name?"
+batch = tokenizer(
+    input, padding=True, truncation=True, max_length=512, return_tensors="pt"
+).to(device)
 
-with torch.no_grad():
-    output=model(**batch)
-    print(output)
+if __name__ == "__main__":
+    with torch.no_grad():
+        output = model(**batch)
+        print(output)
