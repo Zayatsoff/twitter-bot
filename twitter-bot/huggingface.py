8d32da4ad3ff294972454b1395e7681822eb9b83
Zayatsoff <rozin.lior@gmail.com>
2022-04-12 19:56:02 -0400

    Update huggingface.py

--- twitter-bot/huggingface.py
+++ twitter-bot/huggingface.py
@@ -1,23 +1,21 @@
-from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 import torch
+import torch.nn.functional as F
 
-# from torch.functional
+model_name = "mahaamami/distilgpt2-finetuned-wikitext2"
+min_length = 10
+temperature = 0.5
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_name = "hivemind/gpt-j-6B-8bit"
-model = AutoModelForQuestionAnswering.from_pretrained(model_name).to(device)
-tokenizer = AutoTokenizer.from_pretrained(model_name).to(device)
 
-# generator = pipeline("question-answering", model=model, tokenizer=tokenizer).to(device)
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+torch.save(model, f"{model_name}.pt")
+model = torch.load(f"{model_name}.pt").to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# response = generator("What is your name?").to(device)
-
-# Tokenize the response
-input = "What is your name?"
-batch = tokenizer(
-    input, padding=True, truncation=True, max_length=512, return_tensors="pt"
-).to(device)
-
-if __name__ == "__main__":
-    with torch.no_grad():
-        output = model(**batch)
-        print(output)
+gen = pipeline(
+    "text-generation", model=model, tokenizer=tokenizer, framework="pt", device=0
+)
+output = gen(
+    "Fun fact:", do_sample=True, min_length=min_length, temperature=temperature,
+)
+print(output[0]["generated_text"])
